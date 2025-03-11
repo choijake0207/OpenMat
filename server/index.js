4981163d0a5391b17b25bf8716b5be31c4dcb4fa
choijake0207 <jake.ch0712@gmail.com>
2025-03-11 08:02:52 -0700

    set up basic server with socket

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const http = require("http")
+const cors = require("cors")
+const {Server} = require("socket.io")
+
+const app = express()
+app.use(cors)
+app.use(express.json())
+
+// app and socket shared server
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173/",
+        methods: ["GET, POST"]
+    }
+})
+
+// socket set up goes here
+
+
+server.listen(3005, () => {
+    console.log("server listening to port 3005")
+})
+
+
