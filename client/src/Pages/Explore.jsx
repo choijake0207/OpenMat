8e174c82870ca7591eaaf25f1547d166fc540a75
choijake0207 <jake.ch0712@gmail.com>
2025-03-11 12:55:06 -0700

    import necessary components

--- client/src/Pages/Explore.jsx
+++ client/src/Pages/Explore.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import SearchBar from '../Components/SearchBar'
+import Leaflet from '../Components/Leaflet'
+import styles from "../Styles/explore.module.css"
 
 export default function Explore() {
   return (
-    <div>
-      
+    <div className={styles.explore_page}>
+
+      <section className={styles.search_container}>
+        <SearchBar/>
+        <div className={styles.results_container}>
+          <div className={styles.results_filter_bar}></div>
+          <ul className={styles.results_list}></ul>
+        </div>
+      </section>
+
+      <section className={styles.map_container}>
+        <Leaflet/>
+      </section>
+
     </div>
   )
 }
