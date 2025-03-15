f7a2d40c76c1c5b81b956818bad5bdf5f0e725bb
choijake0207 <jake.ch0712@gmail.com>
2025-03-15 10:26:30 -0700

    Fix export statement

--- server/utils/cloudConfig.js
+++ server/utils/cloudConfig.js
@@ -1,5 +1,6 @@
 const cloudinary = require("cloudinary").v2
 const {CloudinaryStorage} = require("multer-storage-cloudinary")
+require("dotenv").config()
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -7,4 +8,4 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 })
 
-module.exports = {cloudinary}
+module.exports = cloudinary
