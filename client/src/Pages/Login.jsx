66845503b7b1acc40dd1f5b6e980da2bdaa44b88
choijake0207 <jake.ch0712@gmail.com>
2025-03-18 13:05:14 -0700

    create all required pages

---
+++ client/src/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginModal from "../Components/LoginModal"
+import SignUpModal from "../Components/SignUpModal"
+
+export default function Login() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
