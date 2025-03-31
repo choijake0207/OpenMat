b282d851730393f10925f56f7bc1a31b17b4835e
choijake0207 <jake.ch0712@gmail.com>
2025-03-31 12:26:50 -0700

    update tile layer style

--- client/src/Components/Leaflet.jsx
+++ client/src/Components/Leaflet.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import {MapContainer, TileLayer, Circle, Marker } from "react-leaflet"
 import "../Styles/map.css"
 import "leaflet/dist/leaflet.css"
+import DynamicMap from './DynamicMap'
 
-export default function Leaflet() {
+export default function Leaflet({center, zoom}) {
   return (
     <div id="Map"> 
-        <MapContainer center={[0,0]} scrollWheelZoom={true} zoom={13}>
+        <MapContainer center={center} scrollWheelZoom={true} zoom={zoom}>
+            <DynamicMap center={center} zoom={zoom}/>
             <TileLayer 
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url='https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token=EJT5SIv3LNhwnPyYYMT3TFPFYMkempYTaff2FmoFUVjKh6PE3EMEBEStdb3xedMT'
+                accessToken="EJT5SIv3LNhwnPyYYMT3TFPFYMkempYTaff2FmoFUVjKh6PE3EMEBEStdb3xedMT"
             />
+          <Marker position={center}/>
         </MapContainer>
       
     </div>
