9e15ef0e0377dde7f218eafe7124c1ed00a1cd82
choijake0207 <jake.ch0712@gmail.com>
2025-03-31 12:15:28 -0700

    create API consumers for geocode and autocomplete APIs

--- client/src/API/GET.js
+++ client/src/API/GET.js
@@ -1,7 +1,10 @@
 import axios from "axios"
 
 const API_URL = "http://localhost:3005"
+const PUBLIC_GEO_KEY="f2bb696f8f9d4c119658400f7d19056a"
 
+
+// fetch user profile[id]
 export const fetchProfile = async(id) => {
     try {
         const response = await axios.get(`${API_URL}/user/profile/${id}`)
@@ -10,3 +13,22 @@ export const fetchProfile = async(id) => {
         throw error
     }
 } 
+// geo auto-complete api
+export const geoAutoComplete = async (text) => {
+    try {
+        const response = await axios.get(`https://api.geoapify.com/v1/geocode/autocomplete?text=${text}&apiKey=${PUBLIC_GEO_KEY}`)
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
+// geo-coder api 
+export const geoLocate = async(text) => {
+    try {
+        const response = await axios.get(`https://api.geoapify.com/v1/geocode/search?text=${text}format=json&apiKey=${PUBLIC_GEO_KEY}`)
+        console.log("API Log", response)
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
