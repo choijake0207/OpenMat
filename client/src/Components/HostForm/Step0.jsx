010eb2f3e8f958fb0d9e6712de1723721582271e
choijake0207 <jake.ch0712@gmail.com>
2025-03-31 20:32:18 -0700

    add icons

--- client/src/Components/HostForm/Step0.jsx
+++ client/src/Components/HostForm/Step0.jsx
@@ -4,24 +4,35 @@ import styles from "../../Styles/hostSetUp.module.css"
 export default function Step0({handleNext}) {
   return (
     <div className={styles.step_0}>
+
         <section className={styles.intro_section}>
-        <div className={styles.section_header}>
-            <h2>1</h2>
-            <h2>Confirm Your Personal Information</h2>
-        </div>
-        <p>
-            Review your profile information and make sure it's accurate. This will be public for all users to see.
-        </p>
+            <div>
+                <h2 className={styles.section_header}>1. Confirm Your Personal Information</h2>
+                <p>
+                    Review your profile information and make sure it's accurate for public view.
+                </p>
+            </div>
+            <img src="/file.png" alt="file_icon"/>
         </section>
 
         <section className={styles.intro_section}>
-        <div className={styles.section_header}>
-            <h2>2</h2>
-            <h2>Add Listing Details</h2>
-        </div>
-        <p>
-            Add 3 or more photos of your mat space plus a description of what you are intending to host the mat space for.
-        </p>
+            <div>
+                <h2 className={styles.section_header}>2. Add Listing Details</h2>
+                <p>
+                    Add 3 or more photos of your mat space plus a brief description your mat space.
+                </p>
+            </div>
+            <img src="/plans.png" alt="plans_icon"/>
+        </section>
+
+        <section className={styles.intro_section}>
+            <div>
+                <h2 className={styles.section_header}>3. Review Your Listing</h2>
+                <p>
+                    Review your listing's details and then publish!
+                </p>            
+            </div>
+            <img src="/confirmation.png" alt="confirm_icon"/>
         </section>
 
         <footer className={styles.step_footer}>
