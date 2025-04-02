3d3f9278bc107337827f0c47c7dde7417255208e
choijake0207 <jake.ch0712@gmail.com>
2025-04-02 09:45:33 -0700

    creat form validator func and add error toggling logic

--- client/src/Components/HostFormSteps/Step2.jsx
+++ client/src/Components/HostFormSteps/Step2.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "../../Styles/hostSetUp.module.css"
 import Location from '../HostFormInputs/Location'
 import Type from '../HostFormInputs/Type'
@@ -7,29 +7,62 @@ import Description from '../HostFormInputs/Description'
 import Schedule from '../HostFormInputs/Schedule'
 import Title from '../HostFormInputs/Title'
 import { useHostFormStore } from '../../Utils/HostFormStore'
+import ErrorModal from '../ErrorModal'
 
-// Need to component-tize form elements
-// Too many inputs and too many rerenders 
-// Use custom hook or zustand store in each component to set form data state
 
 export default function Step2({handleNext, handlePrev}) {
+  const [error, setError] = useState({status: false, message: null})
+  // require data validator
+  const step2Valid = () => {
+    // getState used to avoid triggering entire step2 rerender
+    const data = useHostFormStore.getState().data
+    if (
+        !data.coordinates || 
+        !data.address || 
+        data.description === "" || 
+        data.scheduleType === "" || 
+        data.title === "" || 
+        !data.images ||
+        !data.type
+      ) {
+        setError({status: true, message: "Please fill out all fields"})
+        return false
+    } else if ([...data.images].length < 3) {
+        setError({status: true, message: "Please upload at least 3 images"})
+        return false
+    }
+    return true
+  }
 
+  const checkStep2 = (e) => {
+    e.preventDefault()
+    if (step2Valid()) {
+      handleNext(e)
+    } 
+  }
 
+  const confirmError = (e) => {
+    e.preventDefault()
+    setError({status: false, message: null})
+  }
 
   return (
     <div className={styles.step_2}>
-        <h2>Listing Details</h2>
-        <Title/>
-        <Location/>
-        <Type/>
-        <Images/>
-        <Description/>
-        <Schedule/>
+      {
+        error.status && <ErrorModal type={"Missing Information"} message={error.message} handleConfirm={(e) => confirmError(e)}/>
+      }
+      <h2>Listing Details</h2>
+      <Title/>
+      <Location/>
+      <Type/>
+      <Images/>
+      <Description/>
+      <Schedule/>
 
-        <footer className={styles.step_footer}>
-            <button className={styles.prev_btn} onClick={(e) => handlePrev(e)}>Go Back</button>
-            <button className={styles.next_or_submit_btn} onClick={(e) => handleNext(e)}>Next</button>
-        </footer>
+      <footer className={styles.step_footer}>
+          <button className={styles.prev_btn} onClick={(e) => handlePrev(e)}>Go Back</button>
+          <button className={styles.next_or_submit_btn} onClick={(e) => checkStep2(e)}>Next</button>
+      </footer>
     </div>
   )
 }
