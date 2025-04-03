db2699548038c2cf191f367b9ba5b3d3ba9b7a33
choijake0207 <jake.ch0712@gmail.com>
2025-04-02 18:03:22 -0700

    create listing model

---
+++ server/models/Listing.js
@@ -0,0 +1,51 @@
+module.exports = (sequelize, DataTypes) => {
+    const Listing = sequelize.define("Listing", {
+        id: {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        coordinates: {
+            type: DataTypes.GEOMETRY('POINT'),
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.JSON,
+            allowNull: false,
+        },
+        type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        images: {
+            type: DataTypes.JSON,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        scheduleType: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        scheduleList: {
+            type: DataTypes.JSON,
+            allowNull: true
+        }
+
+    })
+
+    Listing.associate = (models) => {
+        Listing.belongsTo(models.User, {
+            foreginKey: "userId",
+            onDelete: "CASCADE"
+        })
+    }
+
+    return Listing
+}
